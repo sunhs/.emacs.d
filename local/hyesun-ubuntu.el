c5aa7695e1115de36cbdecdfafc400cc761ba72c
sunhs <sun.zhuhs@gmail.com>
2016-12-08 21:07:59 +0800

    ubuntu

--- local/hyesun-ubuntu.el
+++ local/hyesun-ubuntu.el
@@ -131,9 +131,7 @@
 ;; (require 'color-theme)
 ;; (color-theme-initialize)
 (add-to-list 'custom-theme-load-path (concat nonelpa-dir "/themes"))
-(if (display-graphic-p)
-	(load-theme 'tsdh-dark t)
-  (load-theme 'zsh t))
+(load-theme 'zsh t)
 
 ;; --------------------------------------------------------------------------------------------------------------
 ;; jedi
