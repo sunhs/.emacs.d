f7d8a27bf34c039c1715b2f3a92cf3e44e8f5d52
edward zhu <sun.zhuhs@gmail.com>
2017-03-06 14:07:21 +0800

    move functions defined in kbd.el to utils.el

--- local/kbd.el
+++ local/kbd.el
@@ -11,63 +11,11 @@
 (global-set-key (kbd "C-c C-x s") 'replace-string)
 (global-set-key (kbd "C-c C-x <up>") 'enlarge-window-horizontally)
 (global-set-key (kbd "C-c C-x <down>") 'shrink-window-horizontally)
-;; (define-key (current-global-map) [remap newline] 'newline-and-indent)
-
-(defun show-file-path ()
-  "show the path of file in the current buffer"
-  (interactive)
-  (message (buffer-file-name)))
 (global-set-key (kbd "C-c C-x f") 'show-file-path)
-
-(defun show-buffer-name ()
-  "show the buffer name"
-  (interactive)
-  (message (buffer-name)))
 (global-set-key (kbd "C-c C-x b") 'show-buffer-name)
-
-(defun move-beginning-of-first-word ()
-  (interactive)
-  (move-beginning-of-line 1)
-  (while (or
-		  (= (char-after) 9)
-		  (= (char-after) 32))
-	(forward-char)))
 (global-set-key (kbd "C-c C-x w") 'move-beginning-of-first-word)
-
-(defun select-stripped-line ()
-  (interactive)
-  (move-beginning-of-first-word)
-  (set-mark-command nil)
-  (move-end-of-line 1))
 (global-set-key (kbd "C-c C-x l") 'select-stripped-line)
-
-(defun delete-stripped-line ()
-  (interactive)
-  (select-stripped-line)
-  (backward-delete-char-untabify 1))
 (global-set-key (kbd "C-c C-x d") 'delete-stripped-line)
-
-(defun select-line ()
-  (interactive)
-  (move-beginning-of-line 1)
-  (set-mark-command nil)
-  (move-end-of-line 1))
-
-(defun delete-line ()
-  (interactive)
-  (select-line)
-  (backward-delete-char-untabify 1)
-  (backward-delete-char-untabify 1))
 (global-set-key (kbd "C-c C-x x") 'delete-line)
-
-(defun comment-line ()
-  (interactive)
-  (select-line)
-  (comment-region (mark) (point)))
 (global-set-key (kbd "C-c C-x ;") 'comment-line)
-
-(defun uncomment-line ()
-  (interactive)
-  (select-line)
-  (uncomment-region (mark) (point)))
 (global-set-key (kbd "C-c C-x /") 'uncomment-line)
--- local/utils.el
+++ local/utils.el
@@ -16,6 +16,7 @@
 					 (funcall fun))))
 	(funcall fun)))
 
+
 (defun sort-package-list ()
   (if (boundp 'package-selected-packages)
 	  (setq package-selected-packages
@@ -23,3 +24,63 @@
 				  '(lambda (a b)
 					 (string< (symbol-name a) (symbol-name b)))))
 	nil))
+
+
+(defun show-file-path ()
+  "show the path of file in the current buffer"
+  (interactive)
+  (message (buffer-file-name)))
+
+
+(defun show-buffer-name ()
+  "show the buffer name"
+  (interactive)
+  (message (buffer-name)))
+
+
+(defun move-beginning-of-first-word ()
+  (interactive)
+  (move-beginning-of-line 1)
+  (while (or
+		  (= (char-after) 9)
+		  (= (char-after) 32))
+	(forward-char)))
+
+
+(defun select-stripped-line ()
+  (interactive)
+  (move-beginning-of-first-word)
+  (set-mark-command nil)
+  (move-end-of-line 1))
+
+
+(defun delete-stripped-line ()
+  (interactive)
+  (select-stripped-line)
+  (backward-delete-char-untabify 1))
+
+
+(defun select-line ()
+  (interactive)
+  (move-beginning-of-line 1)
+  (set-mark-command nil)
+  (move-end-of-line 1))
+
+
+(defun delete-line ()
+  (interactive)
+  (select-line)
+  (backward-delete-char-untabify 1)
+  (backward-delete-char-untabify 1))
+
+
+(defun comment-line ()
+  (interactive)
+  (select-line)
+  (comment-region (mark) (point)))
+
+
+(defun uncomment-line ()
+  (interactive)
+  (select-line)
+  (uncomment-region (mark) (point)))
