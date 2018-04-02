d83d71d883cbc704a810571b0daea21fb089edb2
hyesun zhu <sun.zhuhs@gmail.com>
2018-04-03 01:04:46 +0800

    package list

--- config/custom.el
+++ config/custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (yasnippet))))
+ '(package-selected-packages
+   (quote
+	(company-anaconda company-quickhelp company-statistics counsel evil-escape evil-mc evil-terminal-cursor-changer fill-column-indicator flycheck markdown-mode math-symbol-lists neotree rainbow-delimiters smex smooth-scrolling spaceline spacemacs-theme sr-speedbar undo-tree web-mode yapfify yasnippet))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
