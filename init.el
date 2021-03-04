889816d2419df4eae5f4642d8f21b4f88cf4046c
hyesun <hyesunzhu@outlook.com>
2021-03-04 15:45:00 +0800

    hide tool bar and scroll bar

--- init.el
+++ init.el
@@ -40,9 +40,9 @@
 (require 'utils)
 
 (setq inhibit-startup-message t)
-;;(tool-bar-mode -1)
+(tool-bar-mode -1)
 (menu-bar-mode -1)
-;;(scroll-bar-mode -1)
+(scroll-bar-mode -1)
 (fset 'yes-or-no-p 'y-or-n-p)
 (show-paren-mode 1)
 (global-hl-line-mode 1)
