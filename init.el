b2a0cec72dadd9dfbe9b9646731513896ac5a5af
hyesun <hyesunzhu@outlook.com>
2021-03-05 18:25:19 +0800

    fix

--- init.el
+++ init.el
@@ -42,7 +42,7 @@
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(scroll-bar-mode -1)
+;; (scroll-bar-mode -1)
 (fset 'yes-or-no-p 'y-or-n-p)
 (show-paren-mode 1)
 (global-hl-line-mode 1)
