9a46aa308f79fb4b4f1359274b5609a44e266d82
zhuhaisheng <zhuhaisheng@bigo.sg>
2022-10-24 14:56:08 +0800

    primarily use vertico/consult/orderless/marginalia/embark

--- config/custom.el
+++ config/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-ui ivy-rich tree-sitter tree-sitter-langs vterm spaceline quelpa quelpa-use-package blamer dap-mode go-mode solaire-mode dockerfile-mode lsp-docker evil-snipe indent-tools gruvbox-theme mini-modeline protobuf-mode spacemacs-theme evil-surround lsp-pyright evil-magit magit yasnippet yaml-mode use-package undo-fu treemacs-projectile treemacs-evil sphinx-doc smex smartparens rainbow-delimiters multiple-cursors flycheck evil-terminal-cursor-changer evil-mc evil-escape doom-themes dired-sidebar counsel-projectile company-statistics company-prescient company-lsp cmake-font-lock clang-format+ blacken))
+   '(embark marginalia orderless consult consult-company consult-lsp consult-projectile vertico vertico-posframe lsp-ui ivy-rich tree-sitter tree-sitter-langs vterm spaceline quelpa quelpa-use-package blamer dap-mode go-mode solaire-mode dockerfile-mode lsp-docker evil-snipe indent-tools gruvbox-theme mini-modeline protobuf-mode spacemacs-theme evil-surround lsp-pyright evil-magit magit yasnippet yaml-mode use-package undo-fu treemacs-projectile treemacs-evil sphinx-doc smex smartparens rainbow-delimiters multiple-cursors flycheck evil-terminal-cursor-changer evil-mc evil-escape doom-themes dired-sidebar counsel-projectile company-statistics company-prescient company-lsp cmake-font-lock clang-format+ blacken))
  '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- config/extra-config.el
+++ config/extra-config.el
@@ -1,21 +1,20 @@
 ;; -*- lexical-binding: t -*-
 ;; -*- mode: emacs-lisp -*-
 
-;; --------------------------------------------------------------------------------------------------------------
-;; evil
+;; ---------------------------------------------------- evil ----------------------------------------------------
 (load "evilize")
-;; killboard
-(load "killboard")
 
-;; dap
+;; -------------------------------------------------- killboar --------------------------------------------------
+;; (load "killboard")
+
+;; ---------------------------------------------------- dap -----------------------------------------------------
 (load "dap")
 
-;; quelpa-use-package
+;; --------------------------------------------- quelpa-use-package ---------------------------------------------
 (require 'quelpa-use-package)
 (setq quelpa-update-melpa-p nil)
 
-;; --------------------------------------------------------------------------------------------------------------
-;; smartparens
+;; ------------------------------------------------- smartparens ------------------------------------------------
 (use-package smartparens
   :config
   (smartparens-global-mode)
@@ -50,52 +49,133 @@
                                sp-point-before-same-p))))
 
 ;; --------------------------------------------------------------------------------------------------------------
-;; ivy / swiper / counsel
-(use-package ivy
-  :config
-  (ivy-mode 1)
-  (setq ivy-initial-inputs-alist nil
-        ivy-use-virtual-buffers t
-        ivy-height 15
-        ivy-extra-directories nil
-        ivy-count-format "%d/%d")
-  (setq ivy-ignore-buffers (append '("*Help*") ivy-ignore-buffers)))
+;; <<<<<<<<<<<<<<O<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< ivy / swiper / counsel >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+;; (use-package ivy
+;;   :config
+;;   (ivy-mode 1)
+;;   (setq ivy-initial-inputs-alist nil
+;;         ivy-use-virtual-buffers t
+;;         ivy-height 15
+;;         ivy-extra-directories nil
+;;         ivy-count-format "%d/%d")
+;;   (setq ivy-ignore-buffers (append '("*Help*") ivy-ignore-buffers)))
 
-(use-package ivy-rich
-  :ensure t
-  :init (ivy-rich-mode 1))
+;; (use-package ivy-rich
+;;   :ensure t
+;;   :init (ivy-rich-mode 1))
 
-(use-package swiper
-  :config
-  (global-set-key (kbd "C-s") 'swiper)
-  (global-set-key (kbd "C-r") 'swiper))
+;; (use-package swiper
+;;   :config
+;;   (global-set-key (kbd "C-s") 'swiper)
+;;   (global-set-key (kbd "C-r") 'swiper))
   
 
-(use-package counsel
+;; (use-package counsel
+;;   :config
+;;   (setq ivy-initial-inputs-alist nil)
+;;   (global-set-key (kbd "M-x") 'counsel-M-x)
+;;   (global-set-key (kbd "C-c C-x y") 'counsel-yank-pop)
+;;   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+;;   (define-key hs-leader-map (kbd "SPC") 'counsel-M-x)
+;;   ;; (define-key hs-leader-map (kbd "y") 'counsel-yank-pop)
+;;   (define-key hs-leader-map "ff" 'counsel-find-file))
+
+;; ------------------------------------------------- projectile -------------------------------------------------
+(use-package projectile
   :config
-  (setq ivy-initial-inputs-alist nil)
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-c C-x y") 'counsel-yank-pop)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-  (define-key hs-leader-map (kbd "SPC") 'counsel-M-x)
-  ;; (define-key hs-leader-map (kbd "y") 'counsel-yank-pop)
-  (define-key hs-leader-map "ff" 'counsel-find-file))
+  (projectile-mode t))
+
+;; (use-package counsel-projectile
+;;   :config
+;;   (counsel-projectile-mode t)
+;;   (define-key hs-leader-map "p" 'projectile-command-map)
+;;   (define-key projectile-command-map "f" 'projectile-find-file)
+;;   (define-key projectile-command-map "d" 'projectile-find-dir)
+;;   (define-key projectile-command-map "p" 'projectile-switch-project)
+;;   (define-key projectile-command-map "b" 'projectile-switch-to-buffer)
+;;   (define-key projectile-command-map "g" 'projectile-ripgrep))
 
 ;; --------------------------------------------------------------------------------------------------------------
-;; projectile
-;; (use-package projectile
-;;   :config
-;;   (projectile-mode t))
+;; <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< vertico / consult / orderless >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+(use-package vertico
+  :init
+  (vertico-mode)
 
-(use-package counsel-projectile
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  ;; (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  ;; (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
   :config
-  (counsel-projectile-mode t)
+  ;; define it here rather than evil, to better switch between ivy/counsel/swiper and vertico/consult
+  (define-key hs-leader-map "ff" 'find-file)
+  (define-key hs-leader-map (kbd "SPC") 'execute-extended-command)
+  )
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+(use-package consult
+  :init (recentf-mode)
+  :config
+  (setq consult-project-function #'projectile-project-root)
+  (global-set-key (kbd "C-s") 'consult-line)
+  (define-key hs-leader-map (kbd "bb") 'consult-buffer)
   (define-key hs-leader-map "p" 'projectile-command-map)
-  (define-key projectile-command-map "f" 'projectile-find-file)
-  (define-key projectile-command-map "d" 'projectile-find-dir)
-  (define-key projectile-command-map "p" 'projectile-switch-project)
-  (define-key projectile-command-map "b" 'projectile-switch-to-buffer)
-  (define-key projectile-command-map "g" 'projectile-ripgrep))
+  (define-key projectile-command-map "f" 'consult-projectile-find-file)
+  (define-key projectile-command-map "d" 'consult-projectile-find-dir)
+  (define-key projectile-command-map "p" 'consult-projectile-switch-project)
+  (define-key projectile-command-map "b" 'consult-projectile-switch-to-buffer)
+  (define-key projectile-command-map "g" 'consult-ripgrep))
+
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)  ;; overridden in company
+        ;; completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode))
+
+(use-package embark
+  :ensure t
+
+  :bind
+  (("M-o" . embark-act)         ;; pick some comfortable binding
+   ;; ("M-d" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
 
 ;; --------------------------------------------------------------------------------------------------------------
 ;; treemacs
@@ -159,12 +239,12 @@
 
 ;; --------------------------------------------------------------------------------------------------------------
 ;; spaceline
-(use-package spaceline
-  :config
-  (require 'spaceline-config)
-  (setq powerline-default-separator nil
-        spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
-  (spaceline-spacemacs-theme))
+;; (use-package spaceline
+;;   :config
+;;   (require 'spaceline-config)
+;;   (setq powerline-default-separator nil
+;;         spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
+;;   (spaceline-spacemacs-theme))
 
 ;; (use-package spaceline-all-the-icons 
 ;;   :after spaceline
@@ -192,7 +272,7 @@
   :diminish
   :config
   (global-company-mode t)
-  (setq completion-styles '(basic substring partial-completion emacs22)
+  (setq completion-styles '(orderless basic substring partial-completion emacs22)
         company-idle-delay 0.1
         company-minimum-prefix-length 2
         company-require-match nil
