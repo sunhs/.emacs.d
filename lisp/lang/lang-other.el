9e3da16996823fcb57521392839ed82bcd1c1102
zhuhaisheng <zhuhaisheng@bigo.sg>
2022-12-01 13:45:49 +0800

    format protobuf

--- lisp/lang/lang-other.el
+++ lisp/lang/lang-other.el
@@ -1,5 +1,13 @@
 (use-package cmake-mode)
+
 (use-package protobuf-mode)
+(use-package clang-format+
+  :config
+  (add-hook 'protobuf-mode-hook
+            #'(lambda ()
+                (setq clang-format-style "{ IndentWidth: 4, BasedOnStyle: google, AlignConsecutiveAssignments: true }")
+                )
+            )
+  )
 
 (provide 'lang-other)
-
