1a086184e80b1787567002cc9522196fc77515d9
zhuhaisheng <zhuhaisheng@bigo.sg>
2022-12-15 16:09:01 +0800

    format utils.el

--- lisp/utils.el
+++ lisp/utils.el
@@ -11,26 +11,23 @@
    For client frames to work normally, `fun should explicitly
    turn on or off graphic features."
   (if (daemonp)
-      (let ((fun fun))
-        (add-hook 'after-make-frame-functions
-                  (lambda (frame)
-                    (select-frame frame)
-                    (funcall fun))))
+    (let ((fun fun))
+      (add-hook 'after-make-frame-functions
+        (lambda (frame) (select-frame frame) (funcall fun))))
     (funcall fun)))
 
 
 (defun hs/sort-package-list ()
   (if (boundp 'package-selected-packages)
-      (sort package-selected-packages
-            (lambda (a b)
-              (string< (symbol-name a) (symbol-name b))))
+    (sort
+      package-selected-packages
+      (lambda (a b) (string< (symbol-name a) (symbol-name b))))
     nil))
 
 
 (defmacro hs/define-key-when-set (set-sym key-map key-seq func)
-  (if (and (boundp set-sym)
-           (symbol-value set-sym))
-      `(define-key ,key-map ,key-seq ,func)
+  (if (and (boundp set-sym) (symbol-value set-sym))
+    `(define-key ,key-map ,key-seq ,func)
     nil))
 
 
@@ -51,23 +48,29 @@
 These buffer names start with alphanumeric."
   (interactive)
   (dolist (b (buffer-list))
-    (if (= ?w
-           (char-syntax (aref (buffer-name b) 0)))
-        (kill-buffer b)))
+    (if (= ?w (char-syntax (aref (buffer-name b) 0)))
+      (kill-buffer b)))
   (message "Done."))
 
 
 (defun hs//valid-line-beginning-pos (&optional line)
   (unless line
     (setq line (line-number-at-pos)))
-  (let ((valid-point (line-beginning-position (- 1
-                                                 (- (line-number-at-pos) line)))))
-    (while (or (= (char-after valid-point) 9)
-               (= (char-after valid-point) 32))
+  (let
+    (
+      (valid-point
+        (line-beginning-position
+          (- 1 (- (line-number-at-pos) line)))))
+    (while
+      (or
+        (= (char-after valid-point) 9)
+        (= (char-after valid-point) 32))
       (incf valid-point))
-    (if (or (= (char-after valid-point) 10)
-            (= (char-after valid-point) 13))
-        nil
+    (if
+      (or
+        (= (char-after valid-point) 10)
+        (= (char-after valid-point) 13))
+      nil
       valid-point)))
 
 
@@ -83,7 +86,7 @@ Behaviors:
   (interactive)
   (let ((valid-point (hs//valid-line-beginning-pos)))
     (if valid-point
-        (goto-char valid-point))))
+      (goto-char valid-point))))
 
 
 (defun hs/smart-beginning-of-line ()
@@ -99,9 +102,8 @@ Behaviors:
    Jump to beginning of line."
   (interactive)
   (let ((valid-point (hs//valid-line-beginning-pos)))
-    (if (and valid-point
-             (> (point) valid-point))
-        (goto-char valid-point)
+    (if (and valid-point (> (point) valid-point))
+      (goto-char valid-point)
       (beginning-of-line))))
 
 
@@ -117,10 +119,11 @@ Behaviors:
   (interactive)
   (let ((valid-point (hs//valid-line-beginning-pos)))
     (if valid-point
-        (progn (goto-char valid-point)
-               (push-mark)
-               (activate-mark)
-               (end-of-line)))))
+      (progn
+        (goto-char valid-point)
+        (push-mark)
+        (activate-mark)
+        (end-of-line)))))
 
 
 (defun hs/select-line ()
@@ -135,7 +138,7 @@ Behaviors:
   (interactive)
   (let ((valid-point (hs//valid-line-beginning-pos)))
     (if valid-point
-        (kill-region valid-point (line-end-position)))))
+      (kill-region valid-point (line-end-position)))))
 
 
 (defun hs/kill-whole-line ()
@@ -157,20 +160,25 @@ Behaviors:
 3) Current line is empty:
    Kill whole line."
   (interactive)
-  (let ((valid-point (hs//valid-line-beginning-pos))
-        (del-space-p nil))
+  (let
+    (
+      (valid-point (hs//valid-line-beginning-pos))
+      (del-space-p nil))
     (if valid-point
-        (if (> (point) valid-point)
-            (kill-region valid-point (point))
-          (progn (setq del-space-p t)
-                 (goto-char valid-point)))
-      (progn (setq del-space-p t)
-             (goto-char (line-end-position))))
+      (if (> (point) valid-point)
+        (kill-region valid-point (point))
+        (progn
+          (setq del-space-p t)
+          (goto-char valid-point)))
+      (progn
+        (setq del-space-p t)
+        (goto-char (line-end-position))))
     (if del-space-p
-        (progn (while (not (= (point) (line-beginning-position)))
-                 (backward-delete-char 1))
-               (backward-delete-char 1)
-               (insert " ")))))
+      (progn
+        (while (not (= (point) (line-beginning-position)))
+          (backward-delete-char 1))
+        (backward-delete-char 1)
+        (insert " ")))))
 
 
 (defun hs/comment-line ()
@@ -187,16 +195,16 @@ Behaviors:
 (defun hs/jump-up-half ()
   (interactive)
   (if (> (line-number-at-pos) 1)
-      (goto-line (/ (line-number-at-pos) 2))))
+    (goto-line (/ (line-number-at-pos) 2))))
 
 (defun hs/jump-down-half ()
   (interactive)
-  (let ((distance-to-bottom (-
-                             (line-number-at-pos (point-max))
-                             (line-number-at-pos))
-                            ))
+  (let
+    (
+      (distance-to-bottom
+        (- (line-number-at-pos (point-max)) (line-number-at-pos))))
     (if (> distance-to-bottom 1)
-        (goto-line (+ (line-number-at-pos) (/ distance-to-bottom 2))))))
+      (goto-line (+ (line-number-at-pos) (/ distance-to-bottom 2))))))
 
 (defun hs/term ()
   (interactive)
@@ -208,20 +216,18 @@ Behaviors:
   `pad' should be a char, and will be repeated and equally placed both on the left and right side of `'content'
   if `right-pad' is also a char, use `pad' on the left and `right-pad' on the right"
 
-  (let ((pad-len (/
-                  (-
-                   (- len (length content))
-                   2)
-                  2))
-        (cur-len 0)
-        (result ""))
+  (let
+    (
+      (pad-len (/ (- (- len (length content)) 2) 2))
+      (cur-len 0)
+      (result ""))
     (while (< cur-len pad-len)
       (setq result (concat result pad))
       (incf cur-len))
     (setq result (concat result " " content " "))
     (incf cur-len (+ 2 (length content)))
     (if (not right-pad)
-        (setq right-pad pad))
+      (setq right-pad pad))
     (while (< cur-len len)
       (setq result (concat result right-pad))
       (incf cur-len))
@@ -246,35 +252,45 @@ Behaviors:
 
 (defun hs/find-other-file-in-project (&optional fpath)
   (require 'find-file)
-  (let* ((fname (if fpath
-                    (file-name-nondirectory fpath)
-                  (file-name-nondirectory (buffer-file-name))))
-         (alist (if (symbolp ff-other-file-alist)
-                    (symbol-value ff-other-file-alist)
-                  ff-other-file-alist))
-         (rule (car alist))
-         (pos (ff-string-match (car rule) fname))
-         (stub "")
-         (candidate-fnames nil)
-         (results nil)
-         (nomatch nil)
-         )
+  (let*
+    (
+      (fname
+        (if fpath
+          (file-name-nondirectory fpath)
+          (file-name-nondirectory (buffer-file-name))))
+      (alist
+        (if (symbolp ff-other-file-alist)
+          (symbol-value ff-other-file-alist)
+          ff-other-file-alist))
+      (rule (car alist))
+      (pos (ff-string-match (car rule) fname))
+      (stub "")
+      (candidate-fnames nil)
+      (results nil)
+      (nomatch nil))
 
     ;; find a rule that matches the buffer filename
-    (while (and rule (if (and pos (>= pos 0)) nil (not pos)))
+    (while
+      (and
+        rule
+        (if (and pos (>= pos 0))
+          nil
+          (not pos)))
       (setq alist (cdr alist))
       (setq rule (car alist))
       (setq pos (ff-string-match (car rule) fname)))
 
     (if (not rule)
-        (setq nomatch t)
-      
-      (setq suffixes (car (cdr rule))
-            action (car (cdr rule)))
+      (setq nomatch t)
+
+      (setq
+        suffixes
+        (car (cdr rule))
+        action (car (cdr rule)))
 
       ;; action is a function
       (if (and (atom action) (fboundp action))
-          (setq candidate-fnames (funcall action (ff-buffer-file-name)))
+        (setq candidate-fnames (funcall action (ff-buffer-file-name)))
 
         ;; get the stub (filename without extension)
         (setq format (concat "\\(.+\\)" (car rule)))
@@ -282,45 +298,38 @@ Behaviors:
         (setq stub (match-string 1 fname))
         ;; concat with new suffixes
         (setq candidate-fnames
-              (mapcar (lambda (suffix)
-                        (concat stub suffix))
-                      suffixes))
-        )
-      
+          (mapcar (lambda (suffix) (concat stub suffix)) suffixes)))
+
       ;; find new filenames
       (dolist (candfname candidate-fnames)
         ;; prevent such cases: submodule.cc -> fuck_submodule.cc.o
         (setq regex (concat "^" candfname "$"))
-        (setq results (append results
-                              (directory-files-recursively (projectile-project-root)
-                                                           regex
-                                                           nil
-                                                           nil
-                                                           t))))
-      )
+        (setq results
+          (append
+            results
+            (directory-files-recursively
+              (projectile-project-root)
+              regex
+              nil
+              nil
+              t)))))
 
-    (cond ((or nomatch
-               (not results))
-           (message "no match with `fname' %s and `rule' %s" fname rule)
-           nil)
-          (t
-           results
-           )
-          )
-    )
-  )
+    (cond
+      ((or nomatch (not results))
+        (message "no match with `fname' %s and `rule' %s" fname rule)
+        nil)
+      (t
+        results))))
 
 (defun hs-cmd/find-other-file-in-project ()
   (interactive)
   (let ((results (hs/find-other-file-in-project)))
-    (cond ((not results))
-          ((length= results 1)
-           (find-file (car results)))
-          (t
-           (find-file (completing-read "Open file:" results)))
-          )
-    )
-  )
+    (cond
+      ((not results))
+      ((length= results 1)
+        (find-file (car results)))
+      (t
+        (find-file (completing-read "Open file:" results))))))
 
 (defun hs-cmd/recenter-top ()
   (interactive)
@@ -329,12 +338,14 @@ Behaviors:
   ;;   (recenter-top-bottom 0)
   ;;   (setq scroll-margin old-margin)
   ;;   )
-  (let ((scroll-num (if (> scroll-margin 0)
-                        (- scroll-margin 1)
-                      0)))
+  (let
+    (
+      (scroll-num
+        (if (> scroll-margin 0)
+          (- scroll-margin 1)
+          0)))
     (next-line scroll-num)
-    (recenter-top-bottom 0)
-    )
-  )
+    (recenter-top-bottom 0)))
+
 
 (provide 'utils)
