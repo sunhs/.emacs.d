3026d0428ff4e90f1450dd93ef7ba79d2bbd0aee
zhuhaisheng <zhuhaisheng@bigo.sg>
2023-01-05 18:28:35 +0800

    use diff-hl

--- lisp/extra-config.el
+++ lisp/extra-config.el
@@ -502,15 +502,20 @@
   :config (setq blacken-line-length 120))
 
 ;; https://ianyepan.github.io/posts/emacs-git-gutter/
-(use-package git-gutter
-  :hook (prog-mode . git-gutter-mode)
-  :config (setq git-gutter:update-interval 0.02))
+;; (use-package git-gutter
+;;   :hook (prog-mode . git-gutter-mode)
+;;   :config (setq git-gutter:update-interval 0.02))
 
 ;; (use-package git-gutter-fringe
 ;;   :config
 ;;   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
 ;;   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
 ;;   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+
+(use-package diff-hl
+  :config
+  (global-diff-hl-mode)
+  (diff-hl-margin-mode))
 
 (use-package solaire-mode
   :config (solaire-global-mode t))
