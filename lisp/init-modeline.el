cda500fc1e1a342fdaf9f22c64edc50566183ea2
zhuhaisheng <zhuhaisheng@bigo.sg>
2023-01-07 17:21:25 +0800

    mod mode-line: adjust position; show minor modes in long window

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -11,59 +11,63 @@
   (" %& "
     (:propertize
       (:eval
+        (concat
+          " %l/"
+          (number-to-string (count-lines (point-min) (point-max)))
+          " "))
+      face hs--modeline-line-number-face)
+    " " mode-line-buffer-identification
+    (:propertize
+      (:eval
         (if (derived-mode-p 'eshell-mode)
           ""
           (let ((pname (projectile-project-name)))
             (if pname
               (format "[%s]" pname)
               ""))))
-      face hs--modeline-project-name-face)
-    mode-line-buffer-identification))
+      face hs--modeline-project-name-face)))
 
 (setq hs--modeline-vc-mode '((:eval vc-mode)))
 
-(setq hs--modeline-blank
-  '
-  (
-    (:eval
-      (propertize
-        " " 'display
-        `
-        (
-          (space
-            :align-to
-            (-
-              (+ right right-fringe right-margin)
-              ,
-              (string-width
-                (format-mode-line hs--modeline-rightmost)))))))))
-
-(setq hs--modeline-rightmost
-  '
-  ((:eval mode-name)
-    " "
-    (:propertize
+(defmacro hs--modeline-blank (rightmost)
+  `
+  (quote
+    (
       (:eval
-        (concat
-          " %l/"
-          (number-to-string (count-lines (point-min) (point-max)))
-          " "))
-      face hs--modeline-line-number-face)))
+        (propertize
+          " " 'display
+          `
+          (
+            (space
+              :align-to
+              (-
+                (+ right right-fringe right-margin)
+                ,(string-width (format-mode-line ,rightmost))))))))))
+
+(setq hs--modeline-cur-line '())
+
+(setq hs--modeline-rightmost-short
+  `((:eval mode-name) " " ,hs--modeline-cur-line))
+
+(setq hs--modeline-rightmost-long
+  `((:eval mode-line-modes) " " ,hs--modeline-cur-line))
 
 (defun hs--modeline-long ()
-  (let* ()
-    `
-    (,@hs--modeline-leftmost
-      ,@hs--modeline-vc-mode
-      ,@hs--modeline-blank
-      ,@hs--modeline-rightmost)))
+  `
+  (,@hs--modeline-leftmost
+    ,@hs--modeline-vc-mode
+    "  "
+    ,@
+    (hs--modeline-blank hs--modeline-rightmost-long)
+    ,@hs--modeline-rightmost-long))
 
 (defun hs--modeline-short ()
-  (let* ()
-    `
-    (,@hs--modeline-leftmost
-      ,@hs--modeline-blank
-      ,@hs--modeline-rightmost)))
+  `
+  (,@hs--modeline-leftmost
+    "  "
+    ,@
+    (hs--modeline-blank hs--modeline-rightmost-short)
+    ,@hs--modeline-rightmost-short))
 
 (setq-default mode-line-format
   '
