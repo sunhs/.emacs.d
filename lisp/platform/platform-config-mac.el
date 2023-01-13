74b803eaa52b79a6c4a2b631bd5ab6c190ab6ba7
hyesun <hyesunzhu@outlook.com>
2023-01-13 15:22:58 +0800

    move monitor settings to `hs/call-or-add-to-frame-hook`

--- lisp/platform/platform-config-mac.el
+++ lisp/platform/platform-config-mac.el
@@ -21,38 +21,42 @@
           charset
           (font-spec :family "Microsoft Yahei" :size 14))))))
 
-(let
-  (
-    mon-width
-    (max-width 0)
-    largest-mon-work-area-attr
-    wa-x
-    wa-y
-    wa-width
-    wa-height
-    (ratio 0.9)
-    frame-x
-    frame-y
-    frame-width
-    frame-height)
-  (dolist (mon-attrs (display-monitor-attributes-list))
-    (setq mon-width (nth 3 (car mon-attrs)))
-    (when (> mon-width max-width)
-      (setq max-width mon-width)
-      (setq largest-mon-work-area-attr (cadr mon-attrs))))
-  (setq
-    wa-x
-    (nth 1 largest-mon-work-area-attr)
-    wa-y (nth 2 largest-mon-work-area-attr)
-    wa-width (nth 3 largest-mon-work-area-attr)
-    wa-height (nth 4 largest-mon-work-area-attr)
-    frame-width (floor (* wa-width ratio))
-    frame-height (floor (* wa-height ratio))
-    frame-x (floor (+ (/ (- wa-width frame-width) 2.0) wa-x))
-    frame-y (floor (+ (/ (- wa-height frame-height) 2.0) wa-y)))
-  (set-frame-position (selected-frame) frame-x frame-y)
-  (set-frame-width (selected-frame) frame-width nil t)
-  (set-frame-height (selected-frame) frame-height nil t))
+(hs/call-or-add-to-frame-hook
+  #'
+  (lambda ()
+    (when (display-graphic-p)
+      (let
+        (
+          mon-width
+          (max-width 0)
+          largest-mon-work-area-attr
+          wa-x
+          wa-y
+          wa-width
+          wa-height
+          (ratio 0.9)
+          frame-x
+          frame-y
+          frame-width
+          frame-height)
+        (dolist (mon-attrs (display-monitor-attributes-list))
+          (setq mon-width (nth 3 (car mon-attrs)))
+          (when (> mon-width max-width)
+            (setq max-width mon-width)
+            (setq largest-mon-work-area-attr (cadr mon-attrs))))
+        (setq
+          wa-x
+          (nth 1 largest-mon-work-area-attr)
+          wa-y (nth 2 largest-mon-work-area-attr)
+          wa-width (nth 3 largest-mon-work-area-attr)
+          wa-height (nth 4 largest-mon-work-area-attr)
+          frame-width (floor (* wa-width ratio))
+          frame-height (floor (* wa-height ratio))
+          frame-x (floor (+ (/ (- wa-width frame-width) 2.0) wa-x))
+          frame-y (floor (+ (/ (- wa-height frame-height) 2.0) wa-y)))
+        (set-frame-position (selected-frame) frame-x frame-y)
+        (set-frame-width (selected-frame) frame-width nil t)
+        (set-frame-height (selected-frame) frame-height nil t)))))
 
 ;; to prompt root privileges for non-writable files
 ;; (defadvice find-file (after find-file-sudo activate)
