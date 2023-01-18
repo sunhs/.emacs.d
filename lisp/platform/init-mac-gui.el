e538673a69f21edb6e03537022d6d1716a4af0df
hyesun <hyesunzhu@outlook.com>
2023-01-18 10:14:16 +0800

    larger font on mac

--- lisp/platform/init-mac-gui.el
+++ lisp/platform/init-mac-gui.el
@@ -6,7 +6,7 @@
   mac-command-modifier 'control) ;; map command to control
 
 ;; display chinese fonts normally in GUI
-(set-frame-font "JetBrains Mono 16")
+(set-frame-font "JetBrains Mono 18")
 
 (dolist (charset '(kana han symbol cjk-misc bopomofo))
   (set-fontset-font
